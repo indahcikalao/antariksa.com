549c0e5ed7d58078680427ff9854d5414d1b98ee
indahcikalao <indahcikalao@gmail.com>
2022-12-06 22:22:21 +0700

    Feature : adding admin dashvoard and speed dials

--- src/Pages/AdminDash/AdminDash.jsx
+++ src/Pages/AdminDash/AdminDash.jsx
@@ -1,7 +1,78 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Grid, Container } from '@mui/material';
+import { FaPaperPlane, FaUserAlt } from 'react-icons/fa';
+import { TbReportMoney } from 'react-icons/tb';
+
+import './AdminDash.scss';
 
 function AdminDash() {
-  return <div>AdminDash</div>;
+  return (
+    <div>
+      <section
+        className="bg"
+        style={{ backgroundImage: `url('./img/plane4.jpg')` }}>
+        <Container
+          sx={{
+            pt: 20,
+            pb: 10,
+            textAlign: 'center',
+          }}
+          className="adm-hero">
+          <h1> Hello Admin!</h1>
+          <p>looks what's new in antariksa today!</p>
+        </Container>
+      </section>
+
+      <section className="adm-hero-sect2">
+        <Container sx={{ py: 10 }}>
+          <Grid container justifyContent="center">
+            <Grid item lg={3} md={4} s={8} sx={{ m: 2 }} className="box-info">
+              <Link to="/admin-all-users">
+                <Grid container>
+                  <Grid item md={3} sx={{ mr: 2 }} className="grid">
+                    <FaUserAlt style={{ fontSize: '50px' }} />
+                  </Grid>
+                  <Grid item md={8}>
+                    <h2>Users</h2>
+                    <p>100</p>
+                  </Grid>
+                </Grid>
+              </Link>
+            </Grid>
+
+            <Grid item lg={3} md={4} s={8} sx={{ m: 2 }} className="box-info">
+              <Link to="/admin-add-new-routes">
+                <Grid container>
+                  <Grid item md={3} sx={{ mr: 2 }} className="grid">
+                    <FaPaperPlane style={{ fontSize: '45px' }} />
+                  </Grid>
+                  <Grid item md={8}>
+                    <h2>Routes</h2>
+                    <p>100</p>
+                  </Grid>
+                </Grid>
+              </Link>
+            </Grid>
+
+            <Grid item lg={3} md={4} s={8} sx={{ m: 2 }} className="box-info">
+              <Link to="/admin-all-transactions">
+                <Grid container>
+                  <Grid item md={3} sx={{ mr: 2 }} className="grid">
+                    <TbReportMoney style={{ fontSize: '60px' }} />
+                  </Grid>
+                  <Grid item md={8}>
+                    <h2>Transactions</h2>
+                    <p>100</p>
+                  </Grid>
+                </Grid>
+              </Link>
+            </Grid>
+          </Grid>
+        </Container>
+      </section>
+    </div>
+  );
 }
 
 export default AdminDash;
