66e0350a6427dcd75aed124feee41eb6ac08983c
Indah Cikal Al Gyfary O <109536223+indahcikalao@users.noreply.github.com>
2022-12-08 21:07:06 +0700

    Feature : History Page (#10)

---
+++ src/Components/HistoryCard/HistoryCard.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Grid, Divider, Box } from '@mui/material';
+import { RiPlaneFill } from 'react-icons/ri';
+import { HiOutlineArrowNarrowRight } from 'react-icons/hi';
+import './HistoryCard.scss';
+import { Link } from 'react-router-dom';
+
+export default function HistoryCard() {
+  return (
+    <Grid item md={12} sx={{ mx: 5, mb: 1 }} className="his-grid">
+      <Link to={`/`}>
+        <Divider variant="middle" />
+
+        <Grid container sx={{ pt: 2 }} justifyContent="center" className="coba">
+          <Grid item sm={9} xs={12} className="grid-i-his-card">
+            <span>Booking id</span> 12345678
+          </Grid>
+          <Grid item sm={3} xs={12} className="grid-i-his-card">
+            <Box className="payment">Paid</Box>
+            <Box className="payment not">Not Paid</Box>
+          </Grid>
+        </Grid>
+        <Grid container sx={{ pb: 2 }} justifyContent="center" className="coba">
+          <Grid item sm={9} xs={12} className="grid-i-his-card">
+            <RiPlaneFill style={{ fontSize: '30px', paddingRight: '10px' }} />
+            Jakarta
+            <HiOutlineArrowNarrowRight
+              style={{ fontSize: '30px', padding: '0 10px' }}
+            />
+            Tokyo
+          </Grid>
+          <Grid item sm={3} xs={12} className="grid-i-his-card">
+            <span>IDR</span> 1.500.000
+          </Grid>
+        </Grid>
+      </Link>
+    </Grid>
+  );
+}
--- src/Pages/History/History.jsx
+++ src/Pages/History/History.jsx
@@ -1,7 +1,45 @@
 import React from 'react';
+import { Container, Grid } from '@mui/material';
+import './History.scss';
+import HistoryCard from '../../Components/HistoryCard/HistoryCard';
 
 function History() {
-  return <div>History</div>;
+  return (
+    <div
+      className="bg"
+      style={{
+        backgroundImage: `url('./img/bg-gradient.png')`,
+      }}>
+      <Container
+        className="bg"
+        maxWidth="md"
+        sx={{
+          pt: 20,
+          pb: 10,
+        }}>
+        <div className="box" style={{ minHeight: '400px' }}>
+          <div
+            className="bg-hero-history"
+            style={{
+              backgroundImage: `url('./img/plane5.jpg')`,
+            }}>
+            <h1> Your Transaction History</h1>
+            <p>Here are some flights that you've booked!</p>
+          </div>
+
+          <div>
+            <Grid container justifyContent="center">
+              <HistoryCard />
+              <HistoryCard />
+              <HistoryCard />
+              <HistoryCard />
+              <HistoryCard />
+            </Grid>
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
 }
 
 export default History;
