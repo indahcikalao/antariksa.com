3f37253dd1d803ab23c33db2457be9a02aa4d1a6
Dinda Aisyah Novitasari <100357324+dindanvtsr@users.noreply.github.com>
2022-12-09 09:24:25 +0700

    Feature : pop-up notif, responsive 1024px, fixing return date, etc (#12)

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,15 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./HeaderStyles.css";
 import { BsBellFill } from 'react-icons/bs';
+import { FaBars } from 'react-icons/fa';
+import { FaTimes } from 'react-icons/fa';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import PopupState, { bindTrigger, bindMenu } from 'material-ui-popup-state';
 
 function Header() {
+  const [clicked, setClicked] = useState(false);
+  const handleClick = () => {
+    setClicked(!clicked)
+  }
+
   return (
     <div>
       <nav className='navbarItems'>
         <h1 className='navbarLogo'><a href='/' className='navbarLogo'>antariksa</a></h1>
-        <ul className='navbarMenu'>
+        <div className='menu-icons' onClick={handleClick}>
+
+          {clicked ?
+            <FaTimes style={{cursor: "pointer"}}></FaTimes> :
+            <FaBars style={{cursor: "pointer"}}></FaBars>
+          }
+        </div>
+        <ul className={clicked? 'navbarMenu active' : 'navbarMenu'}>
           <li>
-            <a href='/'><BsBellFill className='notif'/></a>
+            <PopupState variant="popover">
+              {(popupState) => (
+                <React.Fragment>
+                  <BsBellFill className='notif' {...bindTrigger(popupState)}/>
+                  <Menu {...bindMenu(popupState)} sx={{ marginTop : 3 }}>
+                    <MenuItem onClick={popupState.close}>Notification 1</MenuItem>
+                    <MenuItem onClick={popupState.close}>Notification 2</MenuItem>
+                  </Menu>
+                </React.Fragment>
+              )}
+            </PopupState>
           </li>
           <li>
             <a href='/login' className='nav-links-login'>Login</a>
