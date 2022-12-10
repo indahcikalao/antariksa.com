522d8fa62fbe5833e0c1b0dde4e274302b2ca8fd
Indah Cikal Al Gyfary O <109536223+indahcikalao@users.noreply.github.com>
2022-12-10 22:54:38 +0700

    Feature : Transaction page (#13)

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -15,12 +15,13 @@ import Box from '@mui/material/Box';
 import { Button } from '@mui/material';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import { StyledEngineProvider } from '@mui/material/styles';
+import { Link } from 'react-router-dom';
 
 const tesdata = ['Option 1', 'Option 2'];
 
 const theme = createTheme({
   palette: {
-    primary: { main: '#252C35' }
+    primary: { main: '#252C35' },
   },
 });
 
@@ -29,7 +30,7 @@ const datetheme = createTheme({
     MuiInputBase: {
       styleOverrides: {
         root: {
-          backgroundColor: "white",
+          backgroundColor: 'white',
         },
       },
     },
@@ -52,45 +53,63 @@ function Home() {
   const [counter, setCounter] = useState(0);
 
   const disabledButton = () => {
-    setDisabled(true)
-  }
+    setDisabled(true);
+  };
 
   const normalButton = () => {
-    setDisabled(false)
-  }
+    setDisabled(false);
+  };
 
   return (
     <div>
       <div className="container">
-        <video src="./img/bg.mp4" muted autoPlay loop type="video/mp4" id="glass" width="100%"></video>
-        <div className='hero-text'>
+        <video
+          src="./img/bg.mp4"
+          muted
+          autoPlay
+          loop
+          type="video/mp4"
+          id="glass"
+          width="100%"></video>
+        <div className="hero-text">
           <h1>Find Your Ticket Now</h1>
         </div>
-        
-        <div className='chooseticket'>
-          <div className='radio'>
+
+        <div className="chooseticket">
+          <div className="radio">
             <ThemeProvider theme={theme}>
               <FormControl>
                 <RadioGroup
                   row
                   aria-labelledby="demo-row-radio-buttons-group-label"
                   name="row-radio-buttons-group"
-                  defaultValue="Return"
-                >
-                  <FormControlLabel color="primary" value="Return" control={<Radio />} label="One Way" onClick={disabledButton}/>
-                  <FormControlLabel color="primary" value="One Way" control={<Radio />} label="Return" onClick={normalButton}/>
+                  defaultValue="Return">
+                  <FormControlLabel
+                    color="primary"
+                    value="Return"
+                    control={<Radio />}
+                    label="One Way"
+                    onClick={disabledButton}
+                  />
+                  <FormControlLabel
+                    color="primary"
+                    value="One Way"
+                    control={<Radio />}
+                    label="Return"
+                    onClick={normalButton}
+                  />
                 </RadioGroup>
               </FormControl>
             </ThemeProvider>
           </div>
-          <p className='smallLabel'>No. of Passenger</p>
+          <p className="smallLabel">No. of Passenger</p>
           <ThemeProvider theme={theme}>
             <StyledEngineProvider injectFirst>
               <Autocomplete
                 disablePortal
                 id="combo-box-demo"
                 options={tesdata}
-                sx={{ display : 'inline-block', width: 200 }}
+                sx={{ display: 'inline-block', width: 200 }}
                 renderInput={(params) => <TextField {...params} label="From" />}
                 className="jarakbox"
               />
@@ -98,14 +117,16 @@ function Home() {
                 disablePortal
                 id="combo-box-demo"
                 options={tesdata}
-                sx={{ display : 'inline-block', width: 200 }}
+                sx={{ display: 'inline-block', width: 200 }}
                 renderInput={(params) => <TextField {...params} label="To" />}
                 className="jarakbox"
               />
             </StyledEngineProvider>
 
             <ThemeProvider theme={datetheme}>
-              <Box className='departreturn' sx={{display: 'inline-block', marginRight: 1}}>
+              <Box
+                className="departreturn"
+                sx={{ display: 'inline-block', marginRight: 1 }}>
                 <LocalizationProvider dateAdapter={AdapterDayjs}>
                   <MobileDatePicker
                     label="Depart"
@@ -116,7 +137,9 @@ function Home() {
                   />
                 </LocalizationProvider>
               </Box>
-              <Box className='departreturn' sx={{display: 'inline-block', marginRight: 1}}>
+              <Box
+                className="departreturn"
+                sx={{ display: 'inline-block', marginRight: 1 }}>
                 <LocalizationProvider dateAdapter={AdapterDayjs}>
                   <MobileDatePicker
                     label="Return"
@@ -129,62 +152,85 @@ function Home() {
                 </LocalizationProvider>
               </Box>
             </ThemeProvider>
-            <div className='incredecre'>
-              <ButtonGroup style={{height: 55}}>
+            <div className="incredecre">
+              <ButtonGroup style={{ height: 55 }}>
                 <Button
                   disabled={counter <= 0}
                   onClick={() => {
-                    setCounter(counter - 1)
+                    setCounter(counter - 1);
                   }}>
                   -
                 </Button>
                 {<Button disabled>{counter}</Button>}
                 <Button
                   disabled={counter >= 20}
-                  onClick={()=> {
-                    setCounter(counter+1)
+                  onClick={() => {
+                    setCounter(counter + 1);
                   }}>
                   +
                 </Button>
               </ButtonGroup>
             </div>
           </ThemeProvider>
-          <br></br><br></br>
+          <br></br>
+          <br></br>
           <StyledEngineProvider injectFirst>
-            <Button
-              className='searchflight'
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ width: 180, height: 50, fontSize: 17, float: 'right' }}>
-              Search Flight
-            </Button>
+            <Link to={'/search-result'}>
+              <Button
+                className="searchflight"
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ width: 180, height: 50, fontSize: 17, float: 'right' }}>
+                Search Flight
+              </Button>
+            </Link>
           </StyledEngineProvider>
         </div>
       </div>
-      <div className='grid-container'>
+      <div className="grid-container">
         <div className="grid-item">
-          <img src="./img/fast.jpg" width={350} alt='Fast'></img>
+          <img src="./img/fast.jpg" width={350} alt="Fast"></img>
           <h2>Fast</h2>
           <br></br>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque aut ad amet maiores voluptates voluptas illum? Quidem expedita explicabo, enim soluta rem, delectus sequi, aliquid quas ab libero esse odit.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Doloremque
+            aut ad amet maiores voluptates voluptas illum? Quidem expedita
+            explicabo, enim soluta rem, delectus sequi, aliquid quas ab libero
+            esse odit.
+          </p>
         </div>
         <div className="grid-item">
-          <img src="./img/cheap.jpg" width={350} alt='Cheap'></img>
-          <br></br><br></br>
+          <img src="./img/cheap.jpg" width={350} alt="Cheap"></img>
+          <br></br>
+          <br></br>
           <h2>Cheap</h2>
           <br></br>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur facilis libero sit dolores quibusdam voluptas rerum, aliquam labore nulla laborum illo iure veniam numquam ipsam magnam culpa consequatur neque delectus.</p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur
+            facilis libero sit dolores quibusdam voluptas rerum, aliquam labore
+            nulla laborum illo iure veniam numquam ipsam magnam culpa
+            consequatur neque delectus.
+          </p>
         </div>
         <div className="grid-item">
-          <img src="./img/simple.jpg" width={350} alt='Simple' className='simple'></img>
+          <img
+            src="./img/simple.jpg"
+            width={350}
+            alt="Simple"
+            className="simple"></img>
           <h2>Simple</h2>
           <br></br>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum sint natus veritatis quasi eligendi illum quod ullam accusantium a! Cupiditate adipisci optio in officia inventore sequi? Asperiores sunt incidunt ratione.</p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum sint
+            natus veritatis quasi eligendi illum quod ullam accusantium a!
+            Cupiditate adipisci optio in officia inventore sequi? Asperiores
+            sunt incidunt ratione.
+          </p>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default Home;
