f41fbe9ac11908a6ec732b1d1b785fc510f1b153
Dinda Aisyah Novitasari <100357324+dindanvtsr@users.noreply.github.com>
2022-12-16 14:05:38 +0700

    Feature : navbar token, detail history page (#18)

--- src/redux/actions/listairportAction.js
+++ src/redux/actions/listairportAction.js
@@ -7,7 +7,6 @@ const BASE_URL = process.env.REACT_APP_BASE_URL;
 export const getListAirport = () => async (dispatch) => {
   try {
     const { data } = await axios.get(`${BASE_URL}/list-airport`);
-    console.log(data.data);
     dispatch(getListAirportReducer(data.data));
   } catch (error) {
     throw error;
