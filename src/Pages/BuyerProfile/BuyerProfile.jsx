1e0f8e403a7c0c9b31cbb9cf57c527f2330f45fa
Indah Cikal Al Gyfary O <109536223+indahcikalao@users.noreply.github.com>
2022-12-17 21:20:11 +0700

    Feature : fixing css search page (#21)

--- src/Pages/BuyerProfile/BuyerProfile.jsx
+++ src/Pages/BuyerProfile/BuyerProfile.jsx
@@ -1,58 +1,99 @@
-import React, { useState, useEffect } from "react";
-import Button from "@mui/material/Button"
-import { TextField, Select,
+import React, { useState, useEffect } from 'react';
+import Button from '@mui/material/Button';
+import {
+  TextField,
+  Select,
   MenuItem,
   InputLabel,
-  FormControl, } from "@mui/material";
-import "./BuyerProfile.css";
-import { useDispatch, useSelector } from "react-redux"
+  FormControl,
+} from '@mui/material';
+import './BuyerProfile.css';
+import { useDispatch, useSelector } from 'react-redux';
 
 function BuyerProfile() {
+  const { user } = useSelector((state) => state.auth);
+
+  const [name, setName] = useState(null);
+  const [email, setEmail] = useState(null);
+  const [phone, setPhone] = useState(null);
   const [gender, setGender] = useState('');
+
+  useEffect(() => {
+    if (user) {
+      setName(user.name);
+      setEmail(user.email);
+      if (phone) {
+        setPhone(user.phone);
+      }
+      if (gender) {
+        setGender(user.gender);
+      }
+    }
+  }, [user]);
+
   return (
-    <div className="app" style={{ backgroundImage: `url('./img/pesawat.jpg')` }}>
-      <div className="container">
+    <div
+      className="app"
+      style={{ backgroundImage: `url('./img/pesawat.jpg')` }}>
+      {/* {user && setName(user.name)} */}
+      <div className="container prof">
         <div className="app-wrapper">
-        <h1 className="text-center">Profile User</h1>
+          <h1 className="text-center">Profile User</h1>
           <form className="px-3 py-4">
-        {/* <div className="form-group"> */}
-          {/* <label htmlFor="">Name</label> */}
-          {/* <input type="text" className="form-control"placeholder="Search..." /> */}
-          <br></br>
-          <TextField fullWidth label="Full Name" id="fullWidth" />
-          <br></br>
-          <br></br>
-          <TextField fullWidth label="Email" id="fullWidth" />
-          <br></br>
-          <br></br>
-          <TextField fullWidth label="Phone Number" id="fullWidth" /> 
-          <br></br>
-          {/* <TextField id="select" value="10" select>
+            {/* <div className="form-group"> */}
+            {/* <label htmlFor="">Name</label> */}
+            {/* <input type="text" className="form-control"placeholder="Search..." /> */}
+
+            <TextField
+              fullWidth
+              label="Full Name"
+              id="fullWidth"
+              margin="normal"
+              value={name}
+            />
+
+            <TextField
+              fullWidth
+              label="Email"
+              id="fullWidth"
+              margin="normal"
+              value={email}
+            />
+
+            <TextField
+              fullWidth
+              label="Phone Number"
+              id="fullWidth"
+              margin="normal"
+              value={phone}
+            />
+
+            {/* <TextField id="select" value="10" select>
           <MenuItem value="10">Male</MenuItem>
           <MenuItem value="20">Female</MenuItem>
           </TextField> */}
-          <FormControl sx={{ width: 200 }} margin="dense">
-                      <InputLabel id="gender">Gender</InputLabel>
-                      <Select
-                        labelId="gender"
-                        value={gender}
-                        label="Gender"
-                        onChange={(e) => setGender(e.target.value)}>
-                        <MenuItem value="">
-                          <em>None</em>
-                        </MenuItem>
-                        <MenuItem value="Male">Male</MenuItem>
-                        <MenuItem value="Female">Female</MenuItem>
-                      </Select>
-                    </FormControl>
-          <br></br>
-          <br></br>
-          <Button type="submit" variant="contained">
-            save
-          </Button>
-        {/* </div> */}
-      </form>
-              {/* <Form.Group controlId="name">
+            <FormControl sx={{ width: 200 }} margin="dense">
+              <InputLabel id="gender">Gender</InputLabel>
+              <Select
+                labelId="gender"
+                value={gender}
+                label="Gender"
+                onChange={(e) => setGender(e.target.value)}>
+                <MenuItem value="">
+                  <em>None</em>
+                </MenuItem>
+                <MenuItem value="Male">Male</MenuItem>
+                <MenuItem value="Female">Female</MenuItem>
+              </Select>
+            </FormControl>
+            <br></br>
+            <br></br>
+            <Button type="submit" variant="contained">
+              save
+            </Button>
+            {/* </div> */}
+          </form>
+          {/* <Form.Group controlId="name">
                 <Form.Label>Name</Form.Label>
                 <Form.Control
                   type="text"
@@ -71,9 +112,9 @@ function BuyerProfile() {
                 ></Form.Control>
               </Form.Group> */}
         </div>
-        </div>
       </div>
+    </div>
   );
-};
+}
 
 export default BuyerProfile;
--- src/Pages/SearchRes/SearchRes.jsx
+++ src/Pages/SearchRes/SearchRes.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Grid, Button } from '@mui/material';
+import { Grid, Button, Container } from '@mui/material';
 import './SearchRes.css';
 // import SearchCard from './SearchCard';
 
@@ -8,7 +8,9 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useSearchParams } from 'react-router-dom';
 import { getSearchAirport } from '../../redux/actions/listairportAction';
 import { BiTimer } from 'react-icons/bi';
-import { FaPlane } from 'react-icons/fa';
+import { BsArrowRight } from 'react-icons/bs';
+import { FaPlane, FaPaperPlane } from 'react-icons/fa';
+// import { HiArrowLongRight } from 'react-icons/hi2';
 
 function SearchRes() {
   const [searchParams] = useSearchParams();
@@ -23,67 +25,92 @@ function SearchRes() {
     dispatch(getSearchAirport(oa, da, dd));
   }, [oa, da, dd, dispatch]);
 
+  function currencyFormat(num) {
+    return num
+      .toFixed(2)
+      .replace('.', ',')
+      .replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.');
+  }
+
   return (
     <div
       className="searchTicket"
       style={{ backgroundImage: `url('./img/pesawat.jpg')` }}>
-      <div className="searchTicketContainer">
-        {/* {search.map((item, idx) => (
+      <Container maxWidth="md" sx={{ py: 17 }}>
+        <div className="searchTicketContainer">
+          {/* {search.map((item, idx) => (
           <>
             <h1>{item.origin_airport}</h1>
             <SearchCard key={idx} item={item} />
           </>
         ))} */}
-        {search.map((item, idx) => (
-          <div key={idx}>
-            <div className="searchTicketCard">
-              <div className="formLeft">
-                <h2>{item.airlines}</h2>
+          {search.map((item, idx) => (
+            <div key={idx}>
+              <div className="box" style={{ padding: '30px 50px' }}>
+                <h2 className="airlines">
+                  <FaPaperPlane style={{ paddingRight: '10px' }} />
+                  {item.airlines}
+                </h2>
                 <br></br>
-                <Grid container spacing={2}>
+                <Grid container justifyContent="center">
                   <Grid
                     item
-                    xs={2.2}
-                    sx={{ p: 3 }}
-                    justifyContent="center"
-                    style={{ fontSize: '1rem' }}>
-                    {item.origin_airport}
-                    <br></br>
+                    className="gridcenter"
+                    sm={2}
+                    xs={4}
+                    sx={{
+                      flexDirection: 'column',
+                    }}>
+                    <h3>{item.origin_airport}</h3>
                     {item.depature_time}
                   </Grid>
-                  <Grid item xs={2.2} sx={{ p: 3 }} justifyContent="center">
-                    <FaPlane style={{ fontSize: '20px' }} />
-                    <br></br>
-                    <BiTimer style={{ fontSize: '30px' }} /> 2j 30m
+                  <Grid item sm={5} xs={4}>
+                    <div className="gridcenter exceptXs">
+                      <p>
+                        <FaPlane style={{ fontSize: '20px' }} />
+                      </p>
+                      <p
+                        style={{
+                          display: 'flex',
+                          justifyContent: 'center',
+                          alignItems: 'center',
+                        }}>
+                        <BiTimer
+                          style={{ fontSize: '30px', paddingRight: '5px' }}
+                        />
+                        {item.duration_time}
+                      </p>
+                    </div>
+                    <div className="gridcenter onlyXs">
+                      <BsArrowRight />
+                    </div>
                   </Grid>
                   <Grid
                     item
-                    xs={2.2}
-                    sx={{ p: 3 }}
-                    style={{ fontSize: '1rem' }}>
-                    {item.destination_airport}
-                    <br></br>
+                    className="gridcenter"
+                    sm={2}
+                    xs={4}
+                    sx={{
+                      flexDirection: 'column',
+                    }}>
+                    <h3>{item.destination_airport}</h3>
                     {item.arrival_time}
                   </Grid>
-                  {/* <Grid
-                    item
-                    xs={2.2}
-                    sx={{ p: 6 }}
-                    style={{ fontSize: '0.8rem' }}>
-                    <BiTimer /> 2j 30m
-                  </Grid> */}
                   <Grid
                     item
-                    xs={2.2}
-                    justifyContent="center"
-                    sx={{ p: 6 }}
-                    style={{ fontSize: '1rem' }}>
-                    IDR {item.price}
-                    {/* </span> */}
-                    <br></br>
+                    className="gridcenter"
+                    sm={3}
+                    xs={12}
+                    sx={{
+                      flexDirection: 'column',
+                    }}>
+                    <p>
+                      <strong style={{ paddingRight: '5px' }}>IDR</strong>
+                      {currencyFormat(parseInt(item.price))}
+                    </p>
                     <Button
                       variant="contained"
-                      sx={{ mt: 1, width: '100px' }}
+                      sx={{ width: '100px' }}
                       // onClick={() => this.bookNow(flight._id)}
                       // href={"/book/" + flight._id}
                     >
@@ -92,11 +119,11 @@ function SearchRes() {
                   </Grid>
                 </Grid>
               </div>
+              <br></br>
             </div>
-            <br></br>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      </Container>
     </div>
   );
 }
