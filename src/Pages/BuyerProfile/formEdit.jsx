42cb58d66f7e1720a78cfc0830f8831c103c97f7
Indah Cikal Al Gyfary O <109536223+indahcikalao@users.noreply.github.com>
2023-01-02 19:18:51 +0700

    fixing notif bug (#32)

--- src/Pages/BuyerProfile/formEdit.jsx
+++ src/Pages/BuyerProfile/formEdit.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import {
   TextField,
   MenuItem,
@@ -7,22 +7,21 @@ import {
   Select,
   Divider,
   Container,
-  Box,
   Button,
-} from "@mui/material";
-import "./BuyerProfile.css";
-import { useDispatch, useSelector } from "react-redux";
-import { editUser } from "../../redux/actions/authActions";
-import { FaSave } from "react-icons/fa";
-import { MdCancel } from "react-icons/md";
-import { useNavigate } from "react-router";
+} from '@mui/material';
+import './BuyerProfile.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { editUser } from '../../redux/actions/authActions';
+import { FaSave } from 'react-icons/fa';
+import { MdCancel } from 'react-icons/md';
+import { useNavigate } from 'react-router';
 
 function FormEdit() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const { user } = useSelector((state) => state.auth);
-  const [name, setName] = useState("");
-  const [phone, setPhone] = useState("");
+  const [name, setName] = useState('');
+  const [phone, setPhone] = useState('');
   const [gender, setGender] = useState(``);
 
   const handleSubmit = async (e) => {
@@ -35,7 +34,7 @@ function FormEdit() {
     dispatch(
       editUser(data, (status) => {
         if (status === 201) {
-          navigate("/buyer-profile");
+          navigate('/buyer-profile');
         }
       })
     );
@@ -52,11 +51,10 @@ function FormEdit() {
   return (
     <div
       className="app"
-      style={{ backgroundImage: `url('./img/pesawat.jpg')` }}
-    >
+      style={{ backgroundImage: `url('./img/pesawat.jpg')` }}>
       <Container maxWidth="sm" sx={{ pt: 15, pb: 5 }}>
-        <div className="box" style={{ padding: "30px" }}>
-          <h1 style={{ textAlign: "center" }}>Edit Profile</h1>
+        <div className="box" style={{ padding: '30px' }}>
+          <h1 style={{ textAlign: 'center' }}>Edit Profile</h1>
           <Divider />
           <form className="px-3 py-4">
             <TextField
@@ -84,8 +82,7 @@ function FormEdit() {
                 id="fullWidth"
                 value={gender}
                 label="Gender"
-                onChange={(e) => setGender(e.target.value)}
-              >
+                onChange={(e) => setGender(e.target.value)}>
                 <MenuItem value="">
                   <em>None</em>
                 </MenuItem>
@@ -96,10 +93,9 @@ function FormEdit() {
             <div className="flexbutton">
               <Button
                 variant="contained"
-                sx={{ mx: 0.2, width: "50vw" }}
-                onClick={() => navigate("/buyer-profile")}
-              >
-                <MdCancel className="proficon" style={{ fontSize: "16px" }} />{" "}
+                sx={{ mx: 0.2, width: '50vw' }}
+                onClick={() => navigate('/buyer-profile')}>
+                <MdCancel className="proficon" style={{ fontSize: '16px' }} />{' '}
                 Cancel
               </Button>
 
@@ -110,9 +106,8 @@ function FormEdit() {
                   user?.phone === phone
                 }
                 variant="contained"
-                sx={{ mx: 0.2, width: "50vw" }}
-                onClick={handleSubmit}
-              >
+                sx={{ mx: 0.2, width: '50vw' }}
+                onClick={handleSubmit}>
                 <FaSave className="proficon" />
                 Save
               </Button>
--- src/Pages/History/History.jsx
+++ src/Pages/History/History.jsx
@@ -1,11 +1,12 @@
-import React, { useEffect } from "react";
-import { Button, Container, Grid } from "@mui/material";
-import "./History.scss";
-import HistoryCard from "../../Components/HistoryCard/HistoryCard";
-import { useDispatch, useSelector } from "react-redux";
-import { getAllHistory } from "../../redux/actions/transactionAction";
-import { getListAirport } from "../../redux/actions/listairportAction";
-import { Link } from "react-router-dom";
+import React, { useEffect } from 'react';
+import { Button, Container, Grid } from '@mui/material';
+import './History.scss';
+import HistoryCard from '../../Components/HistoryCard/HistoryCard';
+import { useDispatch, useSelector } from 'react-redux';
+import { getAllHistory } from '../../redux/actions/transactionAction';
+import { getListAirport } from '../../redux/actions/listairportAction';
+import { Link } from 'react-router-dom';
+import { getNotif } from '../../redux/actions/notifAction';
 
 function History() {
   const dispatch = useDispatch();
@@ -17,6 +18,7 @@ function History() {
   useEffect(() => {
     dispatch(getAllHistory());
     dispatch(getListAirport());
+    dispatch(getNotif());
   }, [dispatch]);
 
   return (
@@ -24,23 +26,20 @@ function History() {
       className="bg"
       style={{
         backgroundImage: `url('./img/bg-gradient.png')`,
-      }}
-    >
+      }}>
       <Container
         className="bg"
         maxWidth="md"
         sx={{
           pt: 20,
           pb: 10,
-        }}
-      >
-        <div className="box" style={{ minHeight: "400px" }}>
+        }}>
+        <div className="box" style={{ minHeight: '400px' }}>
           <div
             className="bg-hero-history"
             style={{
               backgroundImage: `url('./img/plane5.jpg')`,
-            }}
-          >
+            }}>
             <h1> Your Transaction History</h1>
             <p>Here are some flights that you've booked!</p>
           </div>
@@ -48,22 +47,20 @@ function History() {
           {dataHis?.length === 0 && coba?.length === 0 ? (
             <Container
               sx={{
-                height: "200px",
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-                alignItems: "center",
-                color: "inherit",
-                letterSpacing: "3px",
-              }}
-            >
+                height: '200px',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+                color: 'inherit',
+                letterSpacing: '3px',
+              }}>
               <h2>No Flight History yet!</h2>
               <p>would like to order your first flight with us?</p>
-              <Link to={"/"}>
+              <Link to={'/'}>
                 <Button
                   variant="contained"
-                  sx={{ my: 1, width: "120px", py: 1 }}
-                >
+                  sx={{ my: 1, width: '120px', py: 1 }}>
                   Book Now!
                 </Button>
               </Link>
