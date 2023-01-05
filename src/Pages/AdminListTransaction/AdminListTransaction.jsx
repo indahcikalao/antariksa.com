d1a9e108fd640217a9c073ea9cb9f96a873ba033
indahcikalao <indahcikalao@gmail.com>
2023-01-05 17:30:15 +0700

    transaction list value name

--- src/Pages/AdminListTransaction/AdminListTransaction.jsx
+++ src/Pages/AdminListTransaction/AdminListTransaction.jsx
@@ -1,15 +1,15 @@
-import React, { useEffect } from "react";
-import { Grid, Container } from "@mui/material";
-import { useDispatch, useSelector } from "react-redux";
-import { getListTransaction } from "../../redux/actions/listAction";
-import Paper from "@mui/material/Paper";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TablePagination from "@mui/material/TablePagination";
-import TableRow from "@mui/material/TableRow";
+import React, { useEffect } from 'react';
+import { Grid, Container } from '@mui/material';
+import { useDispatch, useSelector } from 'react-redux';
+import { getListTransaction } from '../../redux/actions/listAction';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
 
 function AdminListTransaction() {
   const dispatch = useDispatch();
@@ -17,40 +17,40 @@ function AdminListTransaction() {
 
   const columnsTransaction = [
     {
-      id: "user",
-      label: "Name",
+      id: 'user',
+      label: 'Name',
       minWidth: 100,
-      format: (value) => value.name,
+      format: (value) => value?.name || 'Data Not Found',
     },
     {
-      id: "flight",
-      label: "Origin Airport",
+      id: 'flight',
+      label: 'Origin Airport',
       minWidth: 100,
-      format: (value) => value?.origin_airport || "Data Not Found",
+      format: (value) => value?.origin_airport || 'Data Not Found',
     },
     {
-      id: "flight",
-      label: "Destination Airport",
+      id: 'flight',
+      label: 'Destination Airport',
       minWidth: 100,
-      format: (value) => value?.destination_airport || "Data Not Found",
+      format: (value) => value?.destination_airport || 'Data Not Found',
     },
     {
-      id: "flight",
-      label: "Departure Date",
+      id: 'flight',
+      label: 'Departure Date',
       minWidth: 100,
-      format: (value) => value?.depature_date || "Data Not Found",
+      format: (value) => value?.depature_date || 'Data Not Found',
     },
     {
-      id: "flight",
-      label: "Departure Time",
+      id: 'flight',
+      label: 'Departure Time',
       minWidth: 100,
-      format: (value) => value?.depature_time || "Data Not Found",
+      format: (value) => value?.depature_time || 'Data Not Found',
     },
     {
-      id: "flight",
-      label: "Arrival Time",
+      id: 'flight',
+      label: 'Arrival Time',
       minWidth: 100,
-      format: (value) => value?.arrival_time || "Data Not Found",
+      format: (value) => value?.arrival_time || 'Data Not Found',
     },
   ];
   useEffect(() => {
@@ -74,24 +74,21 @@ function AdminListTransaction() {
       className="bg"
       style={{
         backgroundImage: `url('./img/bg-gradient.png')`,
-      }}
-    >
+      }}>
       <Container
         className="bg"
         maxWidth="md"
         sx={{
           pt: 20,
           pb: 10,
-        }}
-      >
-        <div className="boxdetail" style={{ minHeight: "400px" }}>
+        }}>
+        <div className="boxdetail" style={{ minHeight: '400px' }}>
           <div
             className="bg-hero-history"
             style={{
               backgroundImage: `url('./img/plane13.jpg')`,
               backgroundPositionY: -250,
-            }}
-          >
+            }}>
             <h1>List Transaction</h1>
             <p>Here is list of transaction on Antariksa!</p>
           </div>
@@ -99,7 +96,7 @@ function AdminListTransaction() {
           <div>
             <Grid container justifyContent="center">
               <Container className="tickets">
-                <Paper sx={{ width: "100%", overflow: "hidden" }}>
+                <Paper sx={{ width: '100%', overflow: 'hidden' }}>
                   <TableContainer sx={{ maxHeight: 440 }}>
                     <Table stickyHeader aria-label="sticky table">
                       <TableHead>
@@ -108,8 +105,7 @@ function AdminListTransaction() {
                             <TableCell
                               key={idx}
                               align={clm.align}
-                              style={{ minWidth: clm.minWidth }}
-                            >
+                              style={{ minWidth: clm.minWidth }}>
                               {clm.label}
                             </TableCell>
                           ))}
@@ -127,8 +123,7 @@ function AdminListTransaction() {
                                 hover
                                 role="checkbox"
                                 tabIndex={-1}
-                                key={x}
-                              >
+                                key={x}>
                                 {columnsTransaction.map((clm, z) => {
                                   const value = row1[clm.id];
                                   return (
