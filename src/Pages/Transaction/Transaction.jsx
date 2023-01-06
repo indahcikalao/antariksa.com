c197d35f0108194aae9859ce2fcb355953acb8c5
unknown <indahcikalao@gmail.com>
2023-01-06 15:12:52 +0700

    Feature : payment

--- src/Pages/Transaction/Transaction.jsx
+++ src/Pages/Transaction/Transaction.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Container, Grid, Button, Box } from "@mui/material";
+import { Container, Grid, Button, Box, Modal, Divider } from "@mui/material";
 import "./Transaction.scss";
 import TransactionTicket from "../../Components/TransactionTicket/TransactionTicket";
 import TransactionPassanger from "../../Components/TransactionPassanger/TransactionPassanger";
@@ -13,6 +13,21 @@ import {
 } from "../../redux/actions/transactionAction";
 import { getListAirport } from "../../redux/actions/listairportAction";
 import { getNotif } from "../../redux/actions/notifAction";
+import { RiPlaneFill } from "react-icons/ri";
+import { HiOutlineArrowNarrowRight } from "react-icons/hi";
+import moment from "moment/moment";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  maxWidth: 400,
+  bgcolor: "background.paper",
+  boxShadow: 24,
+  p: 4,
+  textAlign: "center",
+};
 
 function Transaction() {
   const dispatch = useDispatch();
@@ -20,6 +35,9 @@ function Transaction() {
   const [searchParams] = useSearchParams();
   const id = searchParams.get("flightId");
   const p = searchParams.get("passengers");
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   const [requestData, setRequestData] = useState({
     id: 0,
@@ -44,6 +62,17 @@ function Transaction() {
   // useEffect(() => {
   //   console.log(requestData);
   // }, [requestData]);
+
+  const depDate = moment(ticket.depature_date, "DD-MM-YYYY").format(
+    "DD MMM YYYY"
+  );
+
+  function currencyFormat(num) {
+    return num
+      .toFixed(2)
+      .replace(".", ",")
+      .replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.");
+  }
 
   function handleBook(e) {
     e.preventDefault();
@@ -94,7 +123,7 @@ function Transaction() {
                   fullWidth
                   variant="contained"
                   sx={{ mr: 3, my: 2, py: 1 }}
-                  onClick={handleBook}
+                  onClick={handleOpen}
                 >
                   Book Now!
                 </Button>
@@ -103,6 +132,127 @@ function Transaction() {
           </Grid>
         </Grid>
       </Container>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style} style={{ borderRadius: "13px" }}>
+          <h2 style={{ letterSpacing: "2px", paddingBottom: "3px" }}>
+            Proceed to Payment
+          </h2>
+          <Container className="tickets">
+            <Box>
+              <div style={{ display: "flex", justifyContent: "center" }}>
+                <RiPlaneFill
+                  style={{ fontSize: "30px", paddingRight: "10px" }}
+                />
+                {listAirport.map(
+                  (item, i) =>
+                    ticket.origin_airport === item.code && (
+                      <h3 key={i}>{item.region}</h3>
+                    )
+                )}
+                <HiOutlineArrowNarrowRight
+                  style={{ fontSize: "30px", padding: "0 10px" }}
+                />
+                {listAirport.map(
+                  (item, i) =>
+                    ticket.destination_airport === item.code && (
+                      <h3 key={i}>{item.region}</h3>
+                    )
+                )}
+              </div>
+              <p>
+                {ticket.airlines} · {depDate} · {ticket.depature_time}
+              </p>
+            </Box>
+
+            <Divider />
+
+            <Box sx={{ py: 1 }}>
+              <h4 style={{ paddingBottom: "10 px" }}>Pessenger Details</h4>
+              {requestData.passangers?.map(
+                (item, i) =>
+                  item.name_passenger &&
+                  item.identity_number && (
+                    <p key={i} style={{ textAlign: "left", padding: "2px 0" }}>
+                      {i + 1} . {item.name_passenger} ({item.identity_number})
+                    </p>
+                  )
+              )}
+            </Box>
+
+            <Divider />
+            <div style={{ padding: "15px 0" }}>
+              <Box className="total" style={{ marginRight: 0, color: "grey" }}>
+                {p > 1 ? (
+                  <>
+                    <p style={{ padding: 0 }}>Seats</p>
+                    <p style={{ padding: 0 }}>{p} Seats</p>
+                  </>
+                ) : (
+                  <>
+                    <p style={{ padding: 0 }}>Seat</p>
+                    <p style={{ padding: 0 }}>{p} Seat</p>
+                  </>
+                )}
+              </Box>
+
+              <Box className="total" style={{ marginRight: 0, color: "grey" }}>
+                <p style={{ padding: 0 }}>Price</p>
+                <p style={{ padding: 0 }}>
+                  <span>IDR </span>
+                  {currencyFormat(parseInt(ticket.price))}
+                </p>
+              </Box>
+
+              <Box
+                className="total"
+                style={{ paddingTop: "10px", marginRight: 0, fontSize: "19px" }}
+              >
+                <p className="span" style={{ padding: 0 }}>
+                  Total
+                </p>
+                <p style={{ padding: 0 }}>
+                  <span className="span">IDR</span>{" "}
+                  {currencyFormat(p * ticket.price)}
+                </p>
+              </Box>
+            </div>
+          </Container>
+
+          <div>
+            <Button
+              variant="contained"
+              color="secondary"
+              sx={{ m: 0.5, width: "150px" }}
+              onClick={handleClose}
+            >
+              Change Data
+            </Button>
+            <Button
+              variant="contained"
+              sx={{ m: 0.5, width: "150px" }}
+              onClick={handleBook}
+              // onClick={() => {
+              //   dispatch(
+              //     deleteListRoute(id, (status) => {
+              //       if (status === 201) {
+              //         setId(null);
+              //         navigate("/admin-dashboard");
+              //       }
+              //     })
+              //   );
+              //   setRefetch(true);
+              // }}
+            >
+              Pay
+            </Button>
+          </div>
+        </Box>
+      </Modal>
     </div>
   );
 }
